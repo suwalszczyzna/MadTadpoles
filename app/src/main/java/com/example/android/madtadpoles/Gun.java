bcdd4dd7b0656b718f13dbc995d1e4b958b61f69
yoori85 <32810279+yoori85@users.noreply.github.com>
2017-12-13 19:50:27 +0100

    change onFinish to afterAttack replace tadpole specific methods with generic ones with tadpole parameter code cleanup switched target SDK to 27 replaced variables with tadpole attributes modified access modifiers

--- app/src/main/java/com/example/android/madtadpoles/Gun.java
+++ app/src/main/java/com/example/android/madtadpoles/Gun.java
@@ -9,12 +9,7 @@ class Gun {
     int damage;
     int icon;
 
-    public Gun(){
-        damage = 0;
-        icon = R.drawable.ic_miecz;
-    }
-
-    public Gun(int damage, int icon) {
+    Gun(int damage, int icon) {
         this.damage = damage;
         this.icon = icon;
     }
